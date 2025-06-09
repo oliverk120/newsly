246c2136bf3756e366532a82aa868e2569f5b048
oliverk120 <oliverk120@gmail.com>
2025-06-09 16:25:35 -0400

    Make tombstone party names clickable

---
+++ test/tombstone.test.js
@@ -0,0 +1,38 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const path = require('node:path');
+const { pathToFileURL } = require('node:url');
+
+async function loadModule() {
+  const url = pathToFileURL(path.join(__dirname, '../public/tombstone.js')).href;
+  return import(url);
+}
+
+test('adds links when URLs provided', async () => {
+  const { createTombstone } = await loadModule();
+  const article = {
+    acquiror: 'Acme',
+    seller: 'SellerCo',
+    target: 'Target',
+    acquiror_url: 'http://acme.com',
+    seller_url: 'http://seller.com',
+    target_url: 'http://target.com',
+    transaction_type: 'M&A'
+  };
+  const html = createTombstone(article);
+  assert(html.includes('http://acme.com'));
+  assert(html.includes('http://seller.com'));
+  assert(html.includes('http://target.com'));
+});
+
+test('omits links when URLs missing', async () => {
+  const { createTombstone } = await loadModule();
+  const article = {
+    acquiror: 'Acme',
+    seller: 'SellerCo',
+    target: 'Target',
+    transaction_type: 'M&A'
+  };
+  const html = createTombstone(article);
+  assert(!html.includes('<a'));
+});
