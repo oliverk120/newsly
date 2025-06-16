e2ec8024a2820ace5d39bf54f4365b84dbedb5e1
oliverk120 <oliverk120@gmail.com>
2025-06-16 04:23:31 -0400

    refactor: extract cosine similarity utility

--- routes/articles.js
+++ routes/articles.js
@@ -5,20 +5,7 @@ const configDb = require('../configDb');
 const { getCompleted } = require('../lib/enrichment/steps');
 const createPipeline = require('../lib/enrichment/pipeline');
 
-function cosineSimilarity(a, b) {
-  let dot = 0;
-  let normA = 0;
-  let normB = 0;
-  for (let i = 0; i < a.length; i++) {
-    const x = a[i];
-    const y = b[i];
-    dot += x * y;
-    normA += x * x;
-    normB += y * y;
-  }
-  if (!normA || !normB) return 0;
-  return dot / (Math.sqrt(normA) * Math.sqrt(normB));
-}
+const cosineSimilarity = require('../utils/cosineSimilarity');
 
 const router = express.Router();
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
---
+++ utils/cosineSimilarity.js
@@ -0,0 +1,16 @@
+function cosineSimilarity(a, b) {
+  let dot = 0;
+  let normA = 0;
+  let normB = 0;
+  for (let i = 0; i < a.length; i++) {
+    const x = a[i];
+    const y = b[i];
+    dot += x * y;
+    normA += x * x;
+    normB += y * y;
+  }
+  if (!normA || !normB) return 0;
+  return dot / (Math.sqrt(normA) * Math.sqrt(normB));
+}
+
+module.exports = cosineSimilarity;
